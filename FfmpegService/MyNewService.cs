ce542b70f7846e19a6831a06e97eeb183b6a10f5
hasunwoo <whs1999kr@naver.com>
2024-09-02 17:11:51 +0900

    added logic

--- FfmpegService/MyNewService.cs
+++ FfmpegService/MyNewService.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.ServiceProcess;
+using System.Threading;
 
 namespace FfmpegService
 {
@@ -12,78 +14,212 @@ namespace FfmpegService
         //ffmpeg 경로
         //ffmpeg.exe 경로로 설정해줘야함
         public const string FFMPEG_PATH = @"C:\ffmpeg경로\ffmpeg.exe";
+        //로그경로 설정해줘야함.
+        public static string HEALTH_CHECK_LOG_PATH = @"C:\로그경로\health.log";
     }
 
     internal class MyNewService : ServiceBase
     {
-        //시작할 ffmpeg instance 정보
-        private List<FfmpegProcessOptions> _instanceOptions;
+        //프로세스 관리자
+        private FfmpegProcessManager _manager;
 
-        //돌아가고있는 프로세스들
-        private List<FfmpegProcess> _runningProcesses;
+        public MyNewService(List<FfmpegProcessOptions> instanceOptions, TimeSpan autoRestartTime, string healthCheckFile) {
+            _manager = new FfmpegProcessManager(instanceOptions, healthCheckFile, autoRestartTime, TimeSpan.FromSeconds(1));
+        }
 
-        public MyNewService(List<FfmpegProcessOptions> instanceOptions) {
-            _instanceOptions = instanceOptions;
-            _runningProcesses = new List<FfmpegProcess>();
+        public void Start()
+        {
+            _manager.Start();
+        }
+
+        //프로세스를 재시작
+
+        protected override void OnStart(string[] args)
+        {
+            _manager.Start();
+        }
+
+        protected override void OnStop()
+        {
+            _manager.Stop();
+        }
+    }
+
+    internal class FfmpegProcessInfo
+    {
+        internal FfmpegProcess process;
+        internal DateTime? created; 
+
+        internal FfmpegProcessInfo(FfmpegProcess process, DateTime? created)
+        {
+            this.process = process;
+            this.created = created;
+        }
+    }
+
+    internal class FfmpegProcessManager
+    {
+        //시작할 ffmpeg instance 정보와 프로세스 정보
+        private Dictionary<FfmpegProcessOptions, FfmpegProcessInfo> _processMap;
+        private object _processMapLock = new object();
+
+        //자동 재시작 간격
+        private TimeSpan _autoRestartTime;
+
+        //프로세스 상태 확인 간격
+        private TimeSpan _healthCheckTime;
+
+        //프로세스 상태 로그 파일
+        private string _healthCheckFile;
+
+        //타이머
+        private Timer _timer;
+        private Timer _healthCheckTimer;
+
+        //실행여부
+        private bool _isRunning;
+        private object _timerLock = new object();
+
+        public FfmpegProcessManager(List<FfmpegProcessOptions> instanceOptions, string healthCheckFile, TimeSpan autoRestartTime, TimeSpan healthCheckTime) {
+            _processMap = instanceOptions.ToDictionary(key => key, key => new FfmpegProcessInfo(null, null));
+            _healthCheckFile = healthCheckFile;
+            _autoRestartTime = autoRestartTime;
+            _healthCheckTime = healthCheckTime;
+            _isRunning = false;
+        }
+
+        public bool IsRunning()
+        {
+            lock(_timerLock)
+            {
+                return _isRunning;
+            }
+        }
+
+        public void Start()
+        {
+            lock(_timerLock)
+            {
+                if(!_isRunning)
+                {
+                    _timer = new Timer(TimerCallback, this, TimeSpan.Zero, _autoRestartTime);
+                    _healthCheckTimer = new Timer(HealthCheckTimerCallback, this, TimeSpan.Zero, _healthCheckTime);
+                    _isRunning = true;
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            lock(_timerLock)
+            {
+                if(_isRunning)
+                {
+                    _healthCheckTimer.Dispose();
+                    _healthCheckTimer = null;
+
+                    _timer.Dispose();
+                    _timer = null;
+
+                    StopProcesses();
+                    _isRunning = false;
+                }
+            }
         }
 
         //프로세스들 시작
-        private void startProcesses()
+        private void StartProcesses()
         {
-            lock(_runningProcesses)
+            lock(_processMapLock)
             {
-                foreach(var opt in _instanceOptions)
+                foreach (var key in _processMap.Keys.ToList())
                 {
-                    FfmpegProcess process = new FfmpegProcess(opt);
-                    process.Start();
-                    _runningProcesses.Add(process);
+                    var value = _processMap[key];
+                    //이미 들아가는지 확인
+                    if(value.process == null || !value.process.IsRunning())
+                    {
+                        Console.WriteLine($"StartProcesses(): {key.ToParameterString()}");
+                        var newProcess = new FfmpegProcess(key);
+                        newProcess.Start();
+                        _processMap[key].process = newProcess;
+                        _processMap[key].created= DateTime.Now;
+                    }
                 }
             }
         }
 
         //프로세스를 종료
-        private void stopProcesses()
+        private void StopProcesses()
         {
-            lock(_runningProcesses)
+            lock(_processMapLock)
             {
-                foreach(var process in _runningProcesses)
+                foreach (var key in _processMap.Keys.ToList())
                 {
-                    if(process.IsRunning())
+                    var value = _processMap[key];
+                    //이미 들아가는지 확인
+                    if(value.process != null && value.process.IsRunning())
                     {
-                        process.Stop();
+                        value.process.Stop();
                     }
+                    _processMap[key].process = null;
                 }
-                _runningProcesses.Clear();
             }
         }
 
-        protected override void OnStart(string[] args)
+        private void HealthCheck()
         {
-            startProcesses();
+            lock(_processMapLock)
+            {
+                using(StreamWriter writer = new StreamWriter(_healthCheckFile))
+                {
+                    writer.WriteLine(DateTime.Now);
+                    foreach (KeyValuePair<FfmpegProcessOptions, FfmpegProcessInfo> pair in _processMap)
+                    {
+                        var key = pair.Key;
+                        var value = pair.Value;
+                        if(value.process != null && value.process.IsRunning())
+                        {
+                            if(DateTime.Now - value.created > value.process.Options().Timeout() + TimeSpan.FromSeconds(2))
+                            {
+                                writer.WriteLine(key.ToParameterString());
+                            }
+                        } 
+                    }
+                }
+            }
         }
 
-        protected override void OnStop()
+        private static void TimerCallback(object _state)
         {
-            stopProcesses();
+            var state = (FfmpegProcessManager)_state;
+            state.StartProcesses();
+        }
+
+        private static void HealthCheckTimerCallback(object _state)
+        {
+            var state = (FfmpegProcessManager)_state;
+            state.HealthCheck();
         }
     }
 
     internal class FfmpegProcessOptions
     {
         private List<string> _options;
+        private TimeSpan _timeout;
 
         public FfmpegProcessOptions(List<string> options)
         {
             _options = options;
+            _timeout = TimeSpan.Zero;
         }
 
-        public FfmpegProcessOptions(string inputAddress, string outputAddress)
+        public FfmpegProcessOptions(string inputAddress, string outputAddress, TimeSpan timeout)
         {
             //기본 옵션
             _options = new List<string>
             {
                 "-fflags", "nobuffer",
-                "-i", $"\"{inputAddress}\"",
+                "-i", $"\"{inputAddress}?timeout={timeout.TotalMilliseconds * 1000}\"",
                 "-c:v", "copy",
                 "-f", "mpegts",
                 "-copytb", "0",
@@ -91,6 +227,12 @@ namespace FfmpegService
                 "-probesize", "1M",
                 $"\"{outputAddress}?pkt_size=1316\"",
             };
+            _timeout = timeout;
+        }
+
+        public TimeSpan Timeout()
+        {
+            return _timeout;
         }
 
         public string ToParameterString()
@@ -155,6 +297,11 @@ namespace FfmpegService
             return _process != null && !_process.HasExited;
         }
 
+        public FfmpegProcessOptions Options()
+        {
+            return _options;
+        }
+
         public void WaitForExit()
         {
             if(!IsRunning())
@@ -173,6 +320,6 @@ namespace FfmpegService
             _process.Kill();
             _process.WaitForExit();
             _process = null;
-    }
+        }
     }
 }
--- FfmpegService/Program.cs
+++ FfmpegService/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ServiceProcess;
 
 namespace FfmpegService
@@ -10,16 +11,16 @@ namespace FfmpegService
         /// </summary>
         static void Main()
         {
-            ServiceBase[] ServicesToRun;
-            ServicesToRun = new ServiceBase[]
-            {
-                new MyNewService(new List<FfmpegProcessOptions>
-                {
-                    //필요한 개수만큼 사용가능
-                    new FfmpegProcessOptions("udp://입력아이피:입력포트", "udp://출력아이피:출력포트"),
-                    new FfmpegProcessOptions("udp://입력아이피:입력포트", "udp://출력아이피:출력포트"),
-                })
-            };
+             ServiceBase[] ServicesToRun;
+             ServicesToRun = new ServiceBase[]
+             {
+                 new MyNewService(new List<FfmpegProcessOptions>
+                 {
+                     //필요한 개수만큼 사용가능
+                     new FfmpegProcessOptions("udp://127.0.0.1:12211", "udp://127.0.0.1:32111", TimeSpan.FromSeconds(5)),
+                     new FfmpegProcessOptions("udp://127.0.0.1:12212", "udp://127.0.0.1:32112", TimeSpan.FromSeconds(5)),
+                 }, TimeSpan.FromSeconds(5), Constants.HEALTH_CHECK_LOG_PATH)
+             };
             ServiceBase.Run(ServicesToRun);
         }
     }
